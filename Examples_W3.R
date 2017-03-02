f86328ead0d06dba2b1e4607d3fbf43901ccf39c
Dan K. Hansen <dankh@live.dk>
2017-03-02 12:54:41 +0100

    stuff

--- Examples_W3.R
+++ Examples_W3.R
@@ -61,3 +61,14 @@ table(pred,testing$Species)
 qplot(Petal.Width, Petal.Length, color=predRight, data=testing, main = "newdata Predictions")
 
 #Boosting
+library(ISLR);data("Wage");library(ggplot2);library(caret);
+Wage <- subset(Wage,select = -c(logwage))
+inTrain <- createDataPartition(y=Wage$wage, p=0.7, list = FALSE)
+training <- Wage[inTrain,]; testing <- Wage[-inTrain,]
+
+modFit <- train(wage ~ ., method = 'gbm', data = training, verbose=FALSE)
+print(modFit)
+
+qplot(predict(modFit, testing), wage, data=testing)
+
+#Model Based Prediction
