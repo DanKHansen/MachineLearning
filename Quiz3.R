4990bdc99ed18e355b3db631bc20178c715b97fd
Dan K. Hansen <dankh@live.dk>
2017-03-06 12:57:23 +0100

    stuff

--- Quiz3.R
+++ Quiz3.R
@@ -2,5 +2,17 @@
 library(AppliedPredictiveModeling)
 data("segmentationOriginal")
 library(caret)
+library(rattle)
 
+training <- subset(segmentationOriginal,Case=='Train')
+testing <- subset(segmentationOriginal,Case=='Test')
 
+set.seed(125)
+
+fit <- train(Class ~.,data = training,method='rpart')
+
+fancyRpartPlot(fit$finalModel)
+
+#A: PS, WS, PS, utp
+
+#Question 2
