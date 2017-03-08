f927d4a85496d1dc9f75dcacd7075c1e0d584572
DanKHansen <dankh@live.dk>
2017-03-08 14:12:03 +0100

    stuff

--- Examples_W4.R
+++ Examples_W4.R
@@ -69,3 +69,65 @@ mod1 <- train(wage~.,method='glm', data=training)
 mod2 <- train(wage~.,method='rf', data=training, trControl = trainControl(method='cv'),number=3)
 pred1 <- predict(mod1,testing);pred2 <- predict(mod2,testing)
 qplot(pred1,pred2,color=wage,data=testing)
+#fitting a model that combines predictors
+predDF <- data.frame(pred1,pred2,wage=testing$wage)
+combModFit <- train(wage~.,method='gam',data=predDF)
+combPred <- predict(combModFit,predDF)
+#check errors on testing set
+sqrt(sum((pred1-testing$wage)^2))
+sqrt(sum((pred2-testing$wage)^2))
+sqrt(sum((combPred-testing$wage)^2))
+#predict on validation set
+pred1V <- predict(mod1,validation);pred2V <- predict(mod2,validation)
+predVDF <- data.frame(pred1=pred1V,pred2=pred2V)
+combPredV <- predict(combModFit,predVDF)
+#check errors on validation set
+sqrt(sum((pred1V-validation$wage)^2))
+sqrt(sum((pred2V-validation$wage)^2))
+sqrt(sum((combPredV-validation$wage)^2))
+
+#Forecasting
+library(quantmod)
+from.dat <- as.Date('01/01/08', format='%m/%d/%y')
+to.dat <- as.Date('12/31/13', format='%m/%d/%y')
+getSymbols('GOOG', src='yahoo', from=from.dat, to=to.dat)
+round(head(GOOG),2)
+mGoog <- to.monthly(GOOG)
+googOpen <- Op(mGoog)
+ts1 <- ts(googOpen,frequency = 12)
+plot(ts1,xlab="Years+1",ylab='GOOG')
+#decomposing
+plot(decompose(ts1),xlab='Years+1')
+#Training and test sets
+ts1Train <- window(ts1,start=1,end=5)
+ts1Test <- window(ts1,start=5)
+ts1Train
+plot(ts1Train)
+library(forecast) #adding moving average
+lines(ma(ts1Train,order=3),col='red')
+#Exponential smoothing
+ets1 <- ets(ts1Train,model='MMM')
+fcast <- forecast(ets1)
+plot(fcast); lines(ts1Test,col='red')
+#Accuracy
+round(accuracy(fcast,ts1Test),2)
+
+#Unsupervised Prediction
+data("iris");library(ggplot2)
+inTrain <- createDataPartition(y=iris$Species,p=0.7,list=F)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+dim(training)
+dim(testing)
+
+kMeans1 <- kmeans(subset(training,select=-c(Species)),centers=3)
+training$clusters <- as.factor(kMeans1$cluster)
+qplot(Petal.Width,Petal.Length,color=clusters,data=training)
+#Compare to real
+table(kMeans1$cluster,training$Species)
+#Build predictor
+modFit <- train(clusters~.,data=subset(training,select=-c(Species)),method='rpart')
+table(predict(modFit,training),training$Species)
+#apply on test
+testClusterPred <- predict(modFit,testing)
+table(testClusterPred,testing$Species)
