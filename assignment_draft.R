823c9a80a07fde7de7f6b27f9e11c94f1115326e
DanKHansen <dankh@live.dk>
2017-03-14 14:04:25 +0100

    stuff

--- assignment_draft.R
+++ assignment_draft.R
@@ -1,2 +1,2 @@
-testing <- read.csv('pml-testing.csv',)
-training <- read.csv('pml-training.csv')
+testing <- read.csv('pml-testing.csv',na.strings = c('','NA','#DIV/0!'))
+training <- read.csv('pml-training.csv',na.strings = c('','NA','#DIV/0!'))
